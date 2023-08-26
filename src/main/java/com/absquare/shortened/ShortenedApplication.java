01394ba1fb02428d75d61dd595ab77a66ad96288
Tharwat <a.tharwat100@gmail.com>
2023-08-26 21:26:14 +0300

    Initial Commit

---
+++ src/main/java/com/absquare/shortened/ShortenedApplication.java
@@ -0,0 +1,11 @@
+package com.absquare.shortened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/absquare/shortened/ShortenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absquare.shortened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
